20af737223424c1002c663f5ab5320c4101b3ce9
Dominic Torruellas <dom@domtorr.com>
2019-08-13 10:58:27 -0700

    Added findById - find scheme by id

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,4 +1,11 @@
+const knex = require('knex');
+
+const db = knex(require('../knexfile').development);
+
 module.exports = {
   find: () => db('schemes'),
+  findById: id =>
+    db('schemes')
+      .where({ id })
+      .first(),
 };
-
